03e11bca498cdd603cb4d1f439d0458d74785b36
0x0b4 <adrianloyal022@outlook.com>
2025-02-05 17:57:14 +0800

    Update Save.lua

--- Save.lua
+++ Save.lua
@@ -1,7 +1,7 @@
 local httpService = game:GetService('HttpService')
 
 local SaveManager = {} do
-	SaveManager.Folder = 'LinoriaLibSettings'
+	SaveManager.Folder = 'anti.solutions'
 	SaveManager.Ignore = {}
 	SaveManager.Parser = {
 		Toggle = {
